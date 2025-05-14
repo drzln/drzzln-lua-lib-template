7e701c010c9f4afbc0b77047129051746da7043b
drzln <drzzln@protonmail.com>
2025-05-14 05:22:11 -0300

    init

--- flake.nix
+++ flake.nix
@@ -1,54 +1,53 @@
 # flake.nix
+# flake.nix
 {
-  description = "Lua CLI project with reusable rockspec-based builder";
+  description = "Lua library project with reusable rockspec-based builder";
+
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
   outputs = {nixpkgs, ...}: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
+
     lua = pkgs.lua5_4;
     luaPkgs = pkgs.lua54Packages;
-    buildLuaFromRockspec = {
+
+    buildLuaLibraryFromRockspec = {
       pname,
       version,
-      # rockspec,
       src,
       lua,
-      luaPkgs,
     }:
       pkgs.stdenv.mkDerivation {
         inherit pname version src;
-        buildInputs = [lua luaPkgs.argparse];
+        buildInputs = [lua];
+
         installPhase = ''
-          echo "[+] Installing bin scripts..."
-          mkdir -p $out/bin
-          cp bin/*.lua $out/bin/
-          chmod +x $out/bin/*
           echo "[+] Installing Lua modules..."
           mkdir -p $out/share/lua/5.4
           find src -type f -name '*.lua' -exec cp --parents {} $out/share/lua/5.4/ \;
         '';
-        dontWrapLua = true;
-        LUA_PATH = "$out/share/lua/5.4/?.lua;$out/share/lua/5.4/?/init.lua;${luaPkgs.argparse}/share/lua/5.4/?.lua;;";
+
+        LUA_PATH = "$out/share/lua/5.4/?.lua;$out/share/lua/5.4/?/init.lua;;";
       };
   in {
-    packages.${system}.default = buildLuaFromRockspec {
-      pname = "hello";
+    packages.${system}.default = buildLuaLibraryFromRockspec {
+      pname = "hellolib";
       version = "0.1.0";
-      rockspec = ./hello-0.1-1.rockspec;
       src = ./.;
       inherit lua luaPkgs;
     };
+
     devShells.${system}.default = pkgs.mkShell {
       buildInputs = [
         lua
         pkgs.luarocks
-        luaPkgs.argparse
         luaPkgs.busted
       ];
       shellHook = ''
-        export LUA_PATH="${luaPkgs.argparse}/share/lua/5.4/?.lua;${luaPkgs.argparse}/share/lua/5.4/?/init.lua;;"
-        export LUA_CPATH="${luaPkgs.argparse}/lib/lua/5.4/?.so;;"
+        export LUA_PATH="./src/?.lua;./src/?/init.lua;;"
         eval "$(luarocks path --lua-version=5.4)"
+        echo "Dev shell for Lua library ready. Use 'lua' or 'busted'"
       '';
     };
   };
